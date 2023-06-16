0289091dcde58d1925d0308631eadda63eec99a9
jsinoy <109936671+jsinoy@users.noreply.github.com>
2023-06-16 15:37:57 +0800

    Update UserMustChangePasswordAtNextLogon.ps1

--- UserMustChangePasswordAtNextLogon.ps1
+++ UserMustChangePasswordAtNextLogon.ps1
@@ -1,10 +1,11 @@
 ﻿$users = get-localuser
 foreach ($user in $users) {
-	# Set users password to be changed at next logon
+	# Set users password to be changed at next logon for all users
 	$expUser = [ADSI]"WinNT://localhost/$user,user"
 	$expUser.passwordExpired = 1 
 	$expUser.setinfo()
 	}`
+ # This line below reverts back the changes for default accounts
 WMIC UserAccount Where "Name='Administrator'" Set PasswordExpires=FALSE
 WMIC UserAccount Where "Name='DefaultAccount'" Set PasswordExpires=FALSE
 WMIC UserAccount Where "Name='Guest'" Set PasswordExpires=FALSE
